6bab7695acc304e474f2a92e3998ce80b6b0cf65
shuuuuuny <shuuuuuny+git@gmail.com>
2021-12-24 00:38:58 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_151119) do
+ActiveRecord::Schema.define(version: 2021_12_23_153631) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
