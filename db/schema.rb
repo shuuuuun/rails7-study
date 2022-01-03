c4bc060b7fcb4167b5db8bfdb890aaabfd5bdf97
shuuuuuny <shuuuuuny+git@gmail.com>
2022-01-03 19:16:15 +0900

    message model
    $ bin/rails g model message room:references content:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_101403) do
+ActiveRecord::Schema.define(version: 2022_01_03_101546) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_01_03_101403) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "messages", "rooms"
 end
