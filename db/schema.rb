60216a23fb57056d73b11ca88d85019ab1dffcc2
shuuuuuny <shuuuuuny+git@gmail.com>
2022-01-03 18:49:48 +0900

    scaffold post with tailwind
    $ bin/rails g scaffold post title:string comment:text --force

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_153631) do
+ActiveRecord::Schema.define(version: 2022_01_03_094833) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
