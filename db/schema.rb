610d5e6a2987bd81e349626666b6cd3882ea5dbc
shuuuuuny <shuuuuuny+git@gmail.com>
2022-01-03 19:14:28 +0900

    scaffold room
    $ bin/rails g scaffold room name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_094833) do
+ActiveRecord::Schema.define(version: 2022_01_03_101403) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
