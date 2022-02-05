f7815bd77ddd449d398a37dd65fcc5f1fb77af69
shuuuuuny <shuuuuuny+git@gmail.com>
2022-02-05 16:29:19 +0900

    messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
+
+  after_create_commit -> { broadcast_append_to room }
+  # broadcasts_to :room
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
 
   resources :posts
+
+  resources :rooms do
+    resources :messages
+  end
 end
