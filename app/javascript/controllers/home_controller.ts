1acab1b9a33a71c474abf7a9ed9ccdbc6976e28f
shuuuuuny <shuuuuuny+git@gmail.com>
2022-02-07 00:48:10 +0900

    fix type errors

--- app/javascript/controllers/home_controller.ts
+++ app/javascript/controllers/home_controller.ts
@@ -3,6 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "name", "output" ]
 
+  outputTarget: Element
+  nameTarget: HTMLInputElement
+
   connect() {
     console.log("Hello World!")
     // this.element.textContent = "Hello World!"
--- app/javascript/controllers/reset_form_controller.ts
+++ app/javascript/controllers/reset_form_controller.ts
@@ -1,6 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  // @ts-ignore
+  element: HTMLFormElement // FIXME: ts(2610). rel: https://stackoverflow.com/q/68477998
+  // declare element: HTMLFormElement
+  // get element(): HTMLFormElement
+
   reset() {
     this.element.reset()
   }
