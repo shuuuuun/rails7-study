fb45c0c6768b6072a25d673c745aafaea8e287a2
shuuuuuny <shuuuuuny+git@gmail.com>
2022-02-20 17:36:40 +0900

    debug=true on development

--- app/javascript/controllers/application.ts
+++ app/javascript/controllers/application.ts
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
+import { NODE_ENV } from "env"
+
+// console.log(`NODE_ENV: ${NODE_ENV}`)
 
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
+application.debug = NODE_ENV === "development"
 window.Stimulus   = application
 
 export { application }
