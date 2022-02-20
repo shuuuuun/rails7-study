6d25b6602ca18daf0778f04c4150fdeeea37b26a
shuuuuuny <shuuuuuny+git@gmail.com>
2022-02-20 13:38:57 +0900

    remove json format on controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,12 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: %i[ show edit update destroy ]
 
-  # GET /rooms or /rooms.json
+  # GET /rooms
   def index
     @rooms = Room.all
   end
 
-  # GET /rooms/1 or /rooms/1.json
+  # GET /rooms/1
   def show
   end
 
@@ -19,41 +19,36 @@ class RoomsController < ApplicationController
   def edit
   end
 
-  # POST /rooms or /rooms.json
+  # POST /rooms
   def create
     @room = Room.new(room_params)
 
     respond_to do |format|
       if @room.save
         format.html { redirect_to room_url(@room), notice: "Room was successfully created." }
-        format.json { render :show, status: :created, location: @room }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /rooms/1 or /rooms/1.json
+  # PATCH/PUT /rooms/1
   def update
     respond_to do |format|
       if @room.update(room_params)
         format.html { redirect_to room_url(@room), notice: "Room was successfully updated." }
-        format.json { render :show, status: :ok, location: @room }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /rooms/1 or /rooms/1.json
+  # DELETE /rooms/1
   def destroy
     @room.destroy
 
     respond_to do |format|
       format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
